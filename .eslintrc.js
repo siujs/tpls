07177d9585c11939d29c74301a0db7b3d21ea125
buns <zpli_buns@126.com>
2020-06-18 09:37:25 +0800

    refactor: remove `@siu/cli-config` , and replace it with origin source

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
-const config = require("@siu/cli-config").lint.eslint;
-
-module.exports = config;
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	extends: [
+		"plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+		"prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+		"plugin:prettier/recommended" // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+	],
+	plugins: ["@typescript-eslint", "prettier"],
+	rules: {
+		"@typescript-eslint/no-var-requires": 0,
+		"@typescript-eslint/no-explicit-any": 0,
+		"@typescript-eslint/no-empty-function": 1,
+		"prefer-rest-params": 1,
+		"no-var": 1
+	}
+};
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,15 @@
-const config = require("@siu/cli-rollup").test.jest().lib;
-
-module.exports = config;
+module.exports = {
+	rootDir: ".",
+	globals: {
+		"ts-jest": {
+			tsConfig: "tsconfig.json"
+		}
+	},
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+	transform: {
+		"^.+\\.(ts|tsx)$": "ts-jest"
+	},
+	coverageDirectory: "./coverage",
+	coverageReporters: ["html"],
+	testMatch: ["**/__tests__/**/*.(test|spec).(ts|js)"]
+};
